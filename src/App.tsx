589caf0935d254ddf9015313d1ffc0f69d22f288
masaru514 <415urasam@gmail.com>
2021-09-04 18:44:47 +0900

    update: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,59 @@
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, makeStyles } from '@material-ui/core';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, makeStyles } from '@material-ui/core';
 import { format } from "date-fns"
 import ja from 'date-fns/locale/ja'
 import data from './assets/dummyData'
 
 const useStyles = makeStyles({
   paper: {
-    maxWidth: 1000,
+    maxWidth: 800,
   },
   root: {
     tableLayout: 'fixed',
-    maxWidth: 1000
+    maxWidth: 800,
+    width: 'auto'
   },
-  row: {
-    display: 'table-row',
+  borderNone: {
+    border: 'none'
   },
   tableRow: {
-    borderBottom: '1px solid rgba(224, 224, 224, 1)'
+    borderBottom: '1px solid rgba(224, 224, 224, 1)',
+    '&:last-child' : {
+      borderBottom: 'none'
+    }
   },
   emptyCell: {
-    width: '68px'
+    width: '68px',
+    borderLeft: '1px solid rgba(224, 224, 224, 1)',
+
   },
   datePick: {
     padding: 0,
+    minWidth: '42px',
+    // border: 'none',
+    border: '1px solid rgba(224, 224, 224, 1)',
+    borderBottom: 'none'
   },
   datePickTableCell: {
     textAlign: 'center',
-    borderBottom: 'none',
-    width: '200px'
+    width: '200px',
+    cursor: 'pointer',
+    border: 'none',
+    '&:hover': {
+      transition: '0.3s',
+      backgroundColor: '#aaa'
+    },
   },
   emptyCellTime: {
     padding: 0,
     width: '68px',
+    border: '1px solid rgba(224, 224, 224, 1)',
+    borderBottom: 'none'
+  },
+  emptyCellRow: {
+    borderBottom: '1px solid rgba(224, 224, 224, 1)',
+    '&:last-child': {
+      borderBottom: 'none'
+    }
   },
   tableTimeCols: {
     display: 'flex',
@@ -43,11 +66,30 @@ const useStyles = makeStyles({
     padding: 0,
   },
   tableHeaderRowMonths: {
-    padding: '12px 20px',
+    padding: '12px 18px',
   },
   tableHeaderRowDates: {
     padding: '12px',
     textAlign: 'center',
+  },
+  sunday: {
+    color: '#EF4444'
+  },
+  saturday: {
+    color: '#3B82F6'
+  },
+  sundayBg: {
+    background: '#FEE2E2'
+  },
+  saturdayBg: {
+    background: '#DBEAFE'
+  },
+  disabled: {
+    background: '#eee',
+    cursor: 'auto',
+    '&:hover': {
+      background: '#eee'
+    }
   }
 })
 
@@ -59,7 +101,7 @@ function App(): JSX.Element {
     d.setDate(d.getDate() + i)
 
     // memo:eee 曜日
-    const fullDate = format(d, 'yyyy MM dd', { locale: ja })
+    const fullDate = format(d, 'yyyy MM dd eee', { locale: ja })
     const [year, month, date, day] = fullDate.split(' ')
     return {
       year,
@@ -76,10 +118,16 @@ function App(): JSX.Element {
   // dateが存在すれば可能とする
   // memo: このへんの要件を少し考えたい + タイムラインとの同期をしないと使い勝手が悪い
   const isAbleReservation = (date: number) => {
-    if(date === 3) return '×'
+    if (date === 3) return '×'
     if (date === 2) return '△'
     if (date === 1 || date === 0) return '○'
     return 'エラー'
+  }
+
+  const checkSatOrSun = (day: string) => {
+    if (day === '土') return [classes.saturday, classes.saturdayBg]
+    if (day === '日') return [classes.sunday, classes.sundayBg]
+    return classes.saturday
   }
 
   // 現在表示されている"日にち"の中で重複している月数の計算を行う。
@@ -106,19 +154,20 @@ function App(): JSX.Element {
 
               <TableRow>
                 <TableCell className={classes.emptyCell} />
-                {twoWeeks.map(({ date }) =>
-                  <TableCell className={classes.tableHeaderRowDates}>
-                      {date}
+                {twoWeeks.map(({ date, day }) =>
+                  <TableCell className={`${classes.tableHeaderRowDates}  ${(day === '土' || day === '日')  ? checkSatOrSun(day)[1] : ''}`}>
+                    <div>{date}</div>
+                    <div className={checkSatOrSun(day)[0]}>({day})</div>
                   </TableCell>
                 )}
               </TableRow>
             </TableHead>
             <TableBody>
-              <TableRow className={classes.row}>
+              <TableRow className={classes.borderNone}>
                 <TableCell className={classes.emptyCellTime}>
                   {time.map((col) => (
-                    <TableRow key={col}>
-                      <TableCell>
+                    <TableRow key={col} className={classes.emptyCellRow}>
+                      <TableCell className={classes.emptyCellRow}>
                         {col}
                       </TableCell>
                     </TableRow>
@@ -130,7 +179,7 @@ function App(): JSX.Element {
                     <TableCell className={classes.datePick}>
                       {obj.timeTable.map(i => (
                         <TableRow className={classes.tableRow}>
-                          <TableCell className={classes.datePickTableCell}>
+                          <TableCell className={`${classes.datePickTableCell} ${isAbleReservation(i.list.length) === '×' ? classes.disabled : ''}`}>
                             {isAbleReservation(i.list.length)}
                           </TableCell>
                         </TableRow>
